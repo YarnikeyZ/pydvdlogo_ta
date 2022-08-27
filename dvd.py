c0be3918e6e3dee0566f0e6d75f0f600949c2c41
YarnikeyZ <83335375+YarnikeyZ@users.noreply.github.com>
2022-08-27 14:42:44 +0500

    Close to full re-write
    ye

--- dvd.py
+++ dvd.py
@@ -1,69 +1,59 @@
-from subprocess import getoutput as gout
 from os import get_terminal_size as gts
 from time import sleep as sl
 from random import randint as rd
 
-t_clr = gout("clear")
-c_clr = "\033[0;0m"
+clr = "\033[0;0m\033[H\033[2J\033[3J"
 
-canvas = list(gts())
-canvas[1] += -2
+def draw_rect(posx: int, posy: int, geomx: int, geomy: int, sym: str, color_id: int):
+    """Draws a rectangle with the specified parameters at the specified coordinates."""
+    line = sym*geomx
+    print(f"\033[38;5;{color_id}m", end="")
+    for g_y in range(geomy):
+        print(f"\033[{posy+1+g_y};{posx+1}H{line}")
 
-def goto(pos_x: int, pos_y: int) -> (str or None):
-    return f"\033[{pos_y};{pos_x}H"
+def check_col(pos, size, speed, canvas, rcolor):
+    """Checks collisions with the edges of the screen."""
+    pos += speed
+    if pos + size >= canvas:
+        speed = -speed
+        pos = canvas - size
+        rcolor = rd(1, 15)
+        return pos, speed, rcolor
+    elif pos <= 0:
+        speed = -speed
+        pos = 0
+        rcolor = rd(1, 15)
+        return pos, speed, rcolor
+    return pos, speed, rcolor
 
-def color(id: int, background: bool) -> str:
-    if background:
-        return f"\033[48;5;{id}m"
-    else:
-        return f"\033[38;5;{id}m"
-
-def draw_rect(pos_x: int, pos_y: int, geom_x: int, geom_y: int, sym: str, color_id: int):
-    print(f"{color(color_id, False)}", end="")
-    for g_y in range(geom_y):
-        print(goto(pos_x+1, pos_y+1+g_y)+sym*geom_x)
-
-def render(fps: int, secret: bool) -> None:
-    rsizey = 5
-    rposy = rd(0, canvas[1]-rsizey)
-    rspeedy = 1
-
-    rsizex = rsizey*2
-    rposx = rd(0, canvas[0]-rsizex)
-    rspeedx = rspeedy*2
-    
-    rcolor = rd(1, 15)
-    while True:
-        if not secret:
-            print(f"{t_clr}{goto(0, 0)}{color(0, True)}{color(233, False)}"+','*canvas[0]*canvas[1])
-        draw_rect(rposx, rposy, rsizex, rsizey, '#', rcolor)
-        sl(1/fps)
-        
-        rposy += rspeedy
-        if rposy + rsizey >= canvas[1]:
-            rspeedy = -rspeedy
-            rposy = canvas[1] - rsizey
-            rcolor = rd(1, 15)
-        elif rposy <= 0:
-            rspeedy = -rspeedy
-            rposy = 0
-            rcolor = rd(1, 15)
-
-        rposx += rspeedx
-        if rposx + rsizex >= canvas[0]:
-            rspeedx = -rspeedx
-            rposx = canvas[0] - rsizex
-            rcolor = rd(1, 15)
-        elif rposx <= 0:
-            rspeedx = -rspeedx
-            rposx = 0
-            rcolor = rd(1, 15)
+def render(fps: int) -> None:
+    """Renders an "image" to the screen."""
+    try:
+        canvas = list(gts())
+        canvas[1] += -1
+        rsizey = 5
+        rsizex = rsizey*2
+        rposy = rd(0, canvas[1]-rsizey)
+        rposx = rd(0, canvas[0]-rsizex)
+        rspeedy = 1
+        rspeedx = rspeedy*2
+        rcolor = rd(1, 15)
+        while True:
+            draw_rect(0, 0, canvas[0], canvas[1], ",", 233)
+            draw_rect(rposx, rposy, rsizex, rsizey, "#", rcolor)
+            rposy, rspeedy, rcolor = check_col(rposy, rsizey, rspeedy, canvas[1], rcolor)
+            rposx, rspeedx, rcolor = check_col(rposx, rsizex, rspeedx, canvas[0], rcolor)
+            canvas = list(gts())
+            canvas[1] += -1
+            sl(1/fps)
+    except KeyboardInterrupt:
+        return
 
 def main():
-    try:
-        print(c_clr, t_clr, render(10, False))
-    except KeyboardInterrupt:
-        exit()
+    """A function responsible for initialization"""
+    print(clr, end="")
+    render(10)
+    print(clr, end="")
 
 if __name__ == "__main__":
     main()
