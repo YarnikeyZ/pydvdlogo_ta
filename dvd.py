c48ef5acf4b0147ffac0f6da7323ddcda0a79a44
YarnikeyZ <83335375+YarnikeyZ@users.noreply.github.com>
2022-08-31 20:31:04 +0500

    Update dvd.py

--- dvd.py
+++ dvd.py
@@ -1,30 +1,9 @@
 from os import get_terminal_size as gts
 from time import sleep as sl
 from random import randint as rd
+from pixpile.pixpile import *
 
 clr = "\033[0;0m\033[H\033[2J\033[3J"
-
-def draw_rect(posx: int, posy: int, geomx: int, geomy: int, sym: str, color_id: int):
-    """Draws a rectangle with the specified parameters at the specified coordinates."""
-    line = sym*geomx
-    print(f"\033[38;5;{color_id}m", end="")
-    for g_y in range(geomy):
-        print(f"\033[{posy+1+g_y};{posx+1}H{line}")
-
-def check_col(pos, size, speed, canvas, rcolor):
-    """Checks collisions with the edges of the screen."""
-    pos += speed
-    if pos + size >= canvas:
-        speed = -speed
-        pos = canvas - size
-        rcolor = rd(1, 15)
-        return pos, speed, rcolor
-    elif pos <= 0:
-        speed = -speed
-        pos = 0
-        rcolor = rd(1, 15)
-        return pos, speed, rcolor
-    return pos, speed, rcolor
 
 def render(fps: int) -> None:
     """Renders an "image" to the screen."""
@@ -39,10 +18,12 @@ def render(fps: int) -> None:
         rspeedx = rspeedy*2
         rcolor = rd(1, 15)
         while True:
-            draw_rect(0, 0, canvas[0], canvas[1], ",", 233)
-            draw_rect(rposx, rposy, rsizex, rsizey, "#", rcolor)
-            rposy, rspeedy, rcolor = check_col(rposy, rsizey, rspeedy, canvas[1], rcolor)
-            rposx, rspeedx, rcolor = check_col(rposx, rsizex, rspeedx, canvas[0], rcolor)
+            draw_rectangle(",", 233, 0, 0, canvas[0], canvas[1])
+            draw_rectangle("#", rcolor, rposx, rposy, rsizex, rsizey)
+            rposy, rspeedy, newcolor = check_collision(rposy, rsizey, rspeedy, canvas[1])
+            rposx, rspeedx, newcolor = check_collision(rposx, rsizex, rspeedx, canvas[0])
+            if newcolor:
+                rcolor = rd(1, 15)
             canvas = list(gts())
             canvas[1] += -1
             sl(1/fps)
