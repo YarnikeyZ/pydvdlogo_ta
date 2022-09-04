c74acb479cec6749e131e736d098664dda4ae855
YarnikeyZ <83335375+YarnikeyZ@users.noreply.github.com>
2022-09-04 12:15:58 +0500

    Update dvd.py

--- dvd.py
+++ dvd.py
@@ -12,14 +12,14 @@ def render(fps: int, rect_num: int) -> None:
         canvas[1] += -1
         # rect = [sym[0], color[1], posx[2], posy[3], sizex[4], sizey[5], speedx[6], speedy[7], newcolor[8]]
         rects = []
-        for r in range(1, rect_num):
+        for r in range(1, rect_num+1):
             rects.append([str(r), rd(1, 15), rd(0, canvas[0]), rd(0, canvas[1]), 10, 5, rd(1, 2)*2, rd(1, 2), False])
         while True:
             draw_rectangle(",", 233, 0, 0, canvas[0], canvas[1])
             for rect in rects:
                 draw_rectangle(rect[0], rect[1], rect[2], rect[3], rect[4], rect[5])
-                rect[2], rect[6], rect[8] = check_collision(rect[2], rect[4], rect[6], canvas[0])
-                rect[3], rect[7], rect[8] = check_collision(rect[3], rect[5], rect[7], canvas[1])
+                rect[2], rect[6], rect[8] = check_collision(rect[2], 0, rect[4], canvas[0], rect[6])
+                rect[3], rect[7], rect[8] = check_collision(rect[3], 0, rect[5], canvas[1], rect[7])
                 if rect[8]:
                     rect[1] = rd(1, 255)
             canvas = list(gts())
